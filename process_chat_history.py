0f62747006787438d50955d51324c01182f0ffec
tomw <tom.wong@memverge.com>
2025-09-30 13:30:04 -0700

    add locomo_how_many_conversations

--- process_chat_history.py
+++ process_chat_history.py
@@ -32,6 +32,35 @@ def timestamp_to_obj(ts):
     return(t_obj)
 
 
+def locomo_count_conversations(infile, verbose=False):
+    if verbose:
+        print(f'lcc: loading locomo input file {infile}', file=sys.stderr)
+    with open(infile) as fp:
+        data = json.load(fp)
+    # loop to load every session
+    conv_count = 0
+    section_count = 0
+    done = False
+    while not done:
+        for section in data:
+            section_count += 1
+            if verbose:
+                print(f'lcc: look for next conversation section {section_count}', file=sys.stderr)
+                # print(f'lcc: keys={list(section.keys())}', file=sys.stderr)
+            if 'conversation' in section:
+                conv_count += 1
+                if verbose:
+                    print(f'lcc: found conversation {conv_count}', file=sys.stderr)
+            if verbose:
+                print(f'lcc: finished conversation {conv_count} (2)', file=sys.stderr)
+            if done:
+                break
+        if verbose:
+            print(f'lcc: counted all sections={section_count}', file=sys.stderr)
+        done = True
+    return(conv_count)
+
+
 def load_locomo(infile, start_time=None, conv_num=None, max_messages=None, verbose=False):
     if not start_time:
         start_time = 0
@@ -43,7 +72,7 @@ def load_locomo(infile, start_time=None, conv_num=None, max_messages=None, verbo
     if verbose:
         print(f'll: start_time={start_time} conv_num={conv_num} max_messages={max_messages}', file=sys.stderr)
     if verbose:
-        print(f'loading locomo input file {infile}', file=sys.stderr)
+        print(f'll: loading locomo input file {infile}', file=sys.stderr)
     with open(infile) as fp:
         data = json.load(fp)
     # loop to load every session
@@ -55,8 +84,8 @@ def load_locomo(infile, start_time=None, conv_num=None, max_messages=None, verbo
         for section in data:
             section_count += 1
             if verbose:
-                print(f'look for next conversation section {section_count}', file=sys.stderr)
-                print(f'keys={list(section.keys())}', file=sys.stderr)
+                print(f'll: look for next conversation section {section_count}', file=sys.stderr)
+                # print(f'll: keys={list(section.keys())}', file=sys.stderr)
             if 'conversation' in section:
                 conversation = section['conversation']
                 conv_count += 1
@@ -64,17 +93,17 @@ def load_locomo(infile, start_time=None, conv_num=None, max_messages=None, verbo
                     # user asked to do one specific conversation
                     continue
                 if verbose:
-                    print(f'loading conversation {conv_count}', file=sys.stderr)
+                    print(f'll: loading conversation {conv_count}', file=sys.stderr)
                 for num in range(1, 9999):
                     session_name = f'session_{num}'
                     session_date_name = f'session_{num}_date_time'
                     if session_name not in conversation:
                         # processed all of the sessions in this conversation
                         if verbose:
-                            print(f'finished conversation {conv_count} (1)', file=sys.stderr)
+                            print(f'll: finished conversation {conv_count} (1)', file=sys.stderr)
                         break
                     if verbose:
-                        print(f'loading conversation {conv_count} session {num}', file=sys.stderr)
+                        print(f'll: loading conversation {conv_count} session {num}', file=sys.stderr)
                     messages = conversation[session_name]
                     session_date_str = ''
                     session_date_obj = None
@@ -95,11 +124,11 @@ def load_locomo(infile, start_time=None, conv_num=None, max_messages=None, verbo
                         if start_time:
                             if timestamp_compare(start_time, session_time) > 0:
                                 if verbose:
-                                    print(f'skipping old conversation {conv_count} session {num} time={session_time}', file=sys.stderr)
+                                    print(f'll: skipping old conversation {conv_count} session {num} time={session_time}', file=sys.stderr)
                                 break
                     except Exception:
                         if verbose:
-                            print(f'ERROR: cannot read timestamp of conversation {conv_count} session {num} date={session_date_str}', file=sys.stderr)
+                            print(f'll: ERROR: cannot read timestamp of conversation {conv_count} session {num} date={session_date_str}', file=sys.stderr)
                     for message in messages:
                         if 'text' in message:
                             lines.append(message['text'])
@@ -107,19 +136,19 @@ def load_locomo(infile, start_time=None, conv_num=None, max_messages=None, verbo
                             if max_messages and msg_count >= max_messages:
                                 # user asked to do this many messages only
                                 if verbose:
-                                    print(f'processed max messages={msg_count}', file=sys.stderr)
+                                    print(f'll: processed max messages={msg_count}', file=sys.stderr)
                                 done = True
                                 break
                     if done:
                         break
                 if verbose:
-                    print(f'finished conversation {conv_count} sessions={num}', file=sys.stderr)
+                    print(f'll: finished conversation {conv_count} sessions={num}', file=sys.stderr)
             if verbose:
-                print(f'finished conversation {conv_count} (2)', file=sys.stderr)
+                print(f'll: finished conversation {conv_count} (2)', file=sys.stderr)
             if done:
                 break
         if verbose:
-            print(f'loaded all sections={section_count}', file=sys.stderr)
+            print(f'll: loaded all sections={section_count}', file=sys.stderr)
         done = True
     return(lines)
 
@@ -134,6 +163,7 @@ def get_args():
     parser.add_argument('-t', '--start_time', action='store', help='only read messages after this time either YYYY-MM-DDTHH:MM:SS or secs since epoch')
     parser.add_argument('-n', '--max_messages', action='store', type=int, default=0, help='only read this many messages')
     parser.add_argument('--locomo_conversation', action='store', type=int, default=0, help='load only this conversation')
+    parser.add_argument('--locomo_how_many_conversations', action='store_true', help='how many conversations are in the input file')
     # parser.add_argument('--summarize_every', action='store', type=int, default=0, help='summarize before storing into memmachine, default is 0')
     args = parser.parse_args()
     if args.help:
@@ -163,7 +193,7 @@ def get_args():
 def usage(args):
     prog = os.path.basename(sys.argv[0])
     # print(f'Usage: {prog} [--src <src>] [--infile <chat_history>] [--outfile <parsed_chat>] [--summarize_every <n_messages>] [--start_time <timestamp>')
-    print(f'Usage: {prog} [--src <src>] --infile <chat_history> [--outfile <parsed_chat>] [--start_time <timestamp> [--num_messages <n>] [--locomo_conversation <n>]')
+    print(f'Usage: {prog} [--src <src>] --infile <chat_history> [--outfile <parsed_chat>] [--start_time <timestamp> [--num_messages <n>] [--locomo_conversation <n>] [--locomo_how_many_conversations]')
     print(f'')
     print(f'src: input file format, either locomo or openai')
     print(f'infile: input filename')
@@ -172,14 +202,19 @@ def usage(args):
     print(f'    either YYYY-MM-DDTHH:MM:SS or secs since epoch')
     print(f'num_messages: read only this many messages')
     print(f'locomo_conversation: if input is locomo, load only this conversation')
-    print(f'')
+    print(f'locomo_how_many_conversations: how many conversations are in the input file')
 
 
 if __name__ == '__main__':
     args = get_args()
     args.src = args.src.lower()
+    lines = []
     if args.src == 'locomo':
-        lines = load_locomo(args.infile, args.start_time, args.locomo_conversation, args.max_messages, args.verbose)
+        if args.locomo_how_many_conversations:
+            count = locomo_count_conversations(args.infile, args.verbose)
+            lines = [f'{count}']
+        else:
+            lines = load_locomo(args.infile, args.start_time, args.locomo_conversation, args.max_messages, args.verbose)
     if args.outfile:
         fp = open(args.outfile, 'w')
     else:
